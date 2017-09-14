a2a347363883fc1de8c94fae467855befb2900a8
RoMay <romay.ua@gmail.com>
2017-09-15 00:00:30 +0200

    fix events url

--- src/graphql/queries/eventsPage.js
+++ src/graphql/queries/eventsPage.js
@@ -10,7 +10,7 @@ export default `
   events: fieldEvents {
     ... on NodeModuleEvent {
       title
-      entityUrl {
+      url: entityUrl {
         path
       }
       location: fieldLocation
