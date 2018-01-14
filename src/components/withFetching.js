581ba9fe581b82204e55ebc057607b8e4529e5fb
RoMay <romay.ua@gmail.com>
2018-01-14 21:37:37 +0100

    add props for withFetching

--- src/components/withFetching.js
+++ src/components/withFetching.js
@@ -1,11 +1,19 @@
 // @flow
 
 import * as React from 'react';
+import type { Dispatch } from 'react-redux';
 import { getFetcher, getCurrentRoute } from '../utils';
 
+type Props = {
+  location: {
+    pathname: string,
+  },
+  dispatch: Dispatch,
+  content: any
+};
+
 function withFetching(WrappedComponent: any, routes: any) {
-  return class extends React.Component<> {
-    static defaultProps: any;
+  return class extends React.Component<Props, any> {
     componentDidMount() {
       this.fetchData();
     }
