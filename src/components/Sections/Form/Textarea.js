f52ca6522d7b4c4129ee108246ab2cfc8e344892
Ruslan Novitskyy <rulyanovi@gmail.com>
2018-01-17 16:46:30 +0200

    fix textarea

--- src/components/Sections/Form/Textarea.js
+++ src/components/Sections/Form/Textarea.js
@@ -9,16 +9,16 @@ type Props = {
 
 const Textarea = (props: Props) => {
   if (props.content) {
-    const { fieldRows, fieldName, fieldPlaceholder } = props.content;
+    const { rows, name, placeholder } = props.content;
     return (
       <StyledTextarea
         className='form-group'
       >
         <textarea
           className='form-control form-control-lg'
-          rows={fieldRows && fieldRows.value}
-          placeholder={fieldPlaceholder && fieldPlaceholder.value}
-          name={fieldName && fieldName.value}
+          rows={rows}
+          placeholder={placeholder}
+          name={name}
         />
       </StyledTextarea>
     );
