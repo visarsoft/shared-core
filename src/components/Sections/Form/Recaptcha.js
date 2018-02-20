482d0d31fa76a493dd650b543e55ac37778b8015
RoMay <romay.ua@gmail.com>
2018-02-20 01:24:28 +0100

    load recaptcha

--- src/components/Sections/Form/Recaptcha.js
+++ src/components/Sections/Form/Recaptcha.js
@@ -1,56 +1,73 @@
 // @flow
 
 import * as React from 'react';
-import Recaptcha from 'react-recaptcha';
+import RecaptchaLib from 'react-recaptcha';
 
 type Props = {
-  content: any,
+  content: any
 };
 
-loadCaptcha() {
-	if (typeof document === "undefined") return;
-
-	const scriptTag = document.createElement('script');
-	scriptTag.async = true;
-	scriptTag.deffer = true;
-	scriptTag.type = 'text/javascript';
-	scriptTag.src = 'https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit&hl=de';
-
-	const head = document.getElementsByTagName('head')[0];
-	head.appendChild(scriptTag);
-}
-
-onloadCallback() {
-	this.setState({
-		isCaptchaLoaded: true
-	});
-	console.log('onloadCallback');
-}
-
-verifyCallback() {
-	this.setState({
-		permission: true
-	});
-}
-
-const RecaptchaModule = (props: Props) => {
-	if (props.content) {
-		const { sitekey, size, render } = props.content;
-		return (
-			{
-				typeof document !== "undefined" && this.state.isCaptchaLoaded ?
-					<Recaptcha
-						sitekey={ sitekey }
-						size={size}
-						render={render}
-						verifyCallback={this.verifyCallback.bind(this)}
-						onloadCallback={this.onloadCallback.bind(this)} 
-					/> :
-						this.loadCaptcha()
-			}
-			);
-			}
-			return null;
+type State = {
+  isCaptchaLoaded: boolean,
+  permission: boolean
 };
 
-export default RecaptchaModule;
+class Recaptcha extends React.Component<Props, State> {
+  static loadCaptcha() {
+    const scriptTag = document.createElement('script');
+    scriptTag.async = true;
+    scriptTag.defer = true;
+    scriptTag.type = 'text/javascript';
+    scriptTag.src = 'https://www.google.com/recaptcha/api.js?hl=de';
+
+    const head = document.getElementsByTagName('head')[0];
+    head.appendChild(scriptTag);
+  }
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      isCaptchaLoaded: false,
+      permission: false
+    };
+    this.verifyCallback = this.verifyCallback.bind(this);
+    this.onloadCallback = this.onloadCallback.bind(this);
+  }
+
+  componentDidMount() {
+    Recaptcha.loadCaptcha();
+  }
+
+  onloadCallback() {
+    this.setState({
+      isCaptchaLoaded: true
+    });
+  }
+  onloadCallback: Function;
+
+  verifyCallback() {
+    this.setState({
+      permission: true
+    });
+  }
+
+  verifyCallback: Function;
+
+  render() {
+    if (this.props.content && typeof document !== 'undefined') {
+      const { sitekey, size, render } = this.props.content;
+      return (
+        <RecaptchaLib
+          sitekey={sitekey}
+          size={size}
+          render={render}
+          verifyCallback={this.verifyCallback}
+          onloadCallback={this.onloadCallback}
+        />
+      );
+    }
+    return null;
+  }
+}
+
+export default Recaptcha;
