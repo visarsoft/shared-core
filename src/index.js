5b488b240d51f55f21fb8773cc71a52e1f84d56c
Roman Prystatskyy <roman.prystatskyy@visarsoft.de>
2019-08-21 23:41:07 +0200

    expose admin handler

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export getConfig from './config';
 export proxyHandler from './proxy';
+export adminHandler from './admin';
 export * from './utils';
 export * from './components';
 export default from './content/graphQL';
